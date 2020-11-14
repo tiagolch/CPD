73a01fe26ab13b676d322671ced8252114aaea4d
Tiago Chaves <tiagodevch@gmail.com>
2020-11-14 17:28:49 -0300

    Listagem_chamado e delete_chamado

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('cadastro_tonners/', cadastro_tonners, name='cadastro_tonners'),
     path('abertura_chamado/', abertura_chamado, name='abertura_chamado'),
     path('atualizacao_tonners/<int:id>/', atualizacao_tonners, name='atualizacao_tonners'),
+    path('atualizacao_chamado/<int:id>/', atualizacao_chamado, name='atualizacao_chamado'),
     path('delete_tonners/<int:id>/', delete_tonners, name='delete_tonners'),
-    path('', listagem_tonners, name='listagem_tonners'),
+    path('delete_chamado/<int:id>/', delete_chamado, name='delete_chamado'),
+    path('listagem_tonners', listagem_tonners, name='listagem_tonners'),
+    path('', listagem_chamado, name='listagem_chamado'),
 ]
--- core/views.py
+++ core/views.py
@@ -18,7 +18,7 @@ def abertura_chamado(request):
     form = AberturaForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('/admin')
+        return redirect('')
     return render(request, "abertura_chamado.html", {'form': form})
 
 
@@ -31,6 +31,15 @@ def atualizacao_tonners(request, id):
     return render(request, 'cadastro_tonners.html', {'form': form})
 
 
+def atualizacao_chamado(request, id):
+    chamado = get_object_or_404(Abertura, pk=id)
+    form = AberturaForm(request.POST or None, instance=chamado)
+    if form.is_valid():
+        form.save()
+        return redirect('listagem_chamado')
+    return render(request, 'abertura_chamado.html', {'form': form})
+
+
 def delete_tonners(request, id):
     tonners = get_object_or_404(Tonners, pk=id)
     if request.method == 'POST':
@@ -39,7 +48,21 @@ def delete_tonners(request, id):
     return render(request, 'delete_tonners_confirm.html', {'tonners': tonners})
 
 
+
+def delete_chamado(request, id):
+    chamado = get_object_or_404(Abertura, pk=id)
+    if request.method == 'POST':
+        chamado.delete()
+        return redirect('listagem_chamado')
+    return render(request, 'delete_chamado_confirm.html', {'chamado': chamado})
+
+
 def listagem_tonners(request):
     list = Tonners.objects.all()
     return render(request, "listagem_tonners.html", {'lista': list})
+
+
+def listagem_chamado(request):
+    list = Abertura.objects.all()
+    return render(request, "listagem_chamado.html", {'lista': list})
 
